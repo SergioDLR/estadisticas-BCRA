2c6ea64235acef143027a890385e938274bb78e6
Sergio López Rey <sergiolopezdanielrey@gmail.com>
2024-10-10 15:22:44 -0300

    Fix: Revalidate

--- next-spa/src/app/page.tsx
+++ next-spa/src/app/page.tsx
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
-const stats = await axios.get<{
-  data: {
-    idVariable: number
-    cdSerie: number
-    description: string
-    dateValue: string
-    value: number
-    variation: number
-  }[]
-}>('https://estadisticas-bcra-production.up.railway.app/statsbcra')
+export default async function Home() {
+  const stats = await axios.get<{
+    data: {
+      idVariable: number
+      cdSerie: number
+      description: string
+      dateValue: string
+      value: number
+      variation: number
+    }[]
+  }>('https://estadisticas-bcra-production.up.railway.app/statsbcra')
 
-export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <h1>Principales variables</h1>
